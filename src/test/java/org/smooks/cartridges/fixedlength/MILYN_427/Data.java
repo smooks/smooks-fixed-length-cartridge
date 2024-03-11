640cdeb9a901abdda6a7a04bcf7e05efe6a2cef1
cjmamo <823038+claudemamo@users.noreply.github.com>
2024-03-11 09:48:38 +0100

    chore: prepare for development
    fix: solve errors caused by changes in Smooks dependencies
    
    docs: remove inherited obsolete ISSUE_TEMPLATE directory

--- src/test/java/org/smooks/cartridges/fixedlength/MILYN_427/Data.java
+++ src/test/java/org/smooks/cartridges/fixedlength/MILYN_427/Data.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -43,55 +43,54 @@
 package org.smooks.cartridges.fixedlength.MILYN_427;
 
 /**
- * 
  * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
  */
 public class Data {
 
-	private String number;
-	private String truncated;
-	private String lastname;
-	private String firstname;
-	private String mail;
+    private String number;
+    private String truncated;
+    private String lastname;
+    private String firstname;
+    private String mail;
 
-	public void setNumber(final String lineNr) {
-		this.number = lineNr;
-	}
+    public void setNumber(final String lineNr) {
+        this.number = lineNr;
+    }
 
-	public String getNumber() {
-		return number;
-	}
+    public String getNumber() {
+        return number;
+    }
 
-	public void setTruncated(final String truncated) {
-		this.truncated = truncated;
-	}
+    public void setTruncated(final String truncated) {
+        this.truncated = truncated;
+    }
 
-	public String getTruncated() {
-		return truncated;
-	}
+    public String getTruncated() {
+        return truncated;
+    }
 
-	public void setLastname(final String lastname) {
-		this.lastname = lastname;
-	}
+    public void setLastname(final String lastname) {
+        this.lastname = lastname;
+    }
 
-	public String getLastname() {
-		return lastname;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public void setFirstname(final String firstname) {
-		this.firstname = firstname;
-	}
+    public void setFirstname(final String firstname) {
+        this.firstname = firstname;
+    }
 
-	public String getFirstname() {
-		return firstname;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public void setMail(final String mail) {
-		this.mail = mail;
-	}
+    public void setMail(final String mail) {
+        this.mail = mail;
+    }
 
-	public String getMail() {
-		return mail;
-	}
+    public String getMail() {
+        return mail;
+    }
 
 }
--- src/test/java/org/smooks/cartridges/fixedlength/MILYN_427/MILYN_427_Test.java
+++ src/test/java/org/smooks/cartridges/fixedlength/MILYN_427/MILYN_427_Test.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -62,80 +62,80 @@ import static org.junit.Assert.*;
 
 /**
  * Contributed by Clemens Fuchslocher.
- *  
+ *
  * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
  */
 public class MILYN_427_Test {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MILYN_427_Test.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MILYN_427_Test.class);
 
-	@Test
-	public void test() throws IOException, SAXException {
-		Smooks smooks = null;
+    @Test
+    public void test() throws IOException, SAXException {
+        Smooks smooks = null;
 
-		try {
-			smooks = new Smooks(getClass().getResourceAsStream("/MILYN_427/smooks-config.xml"));
-			smooks.addVisitor(new AfterVisitor() {
-				private Integer n = 0;
+        try {
+            smooks = new Smooks(getClass().getResourceAsStream("/MILYN_427/smooks-config.xml"));
+            smooks.addVisitor(new AfterVisitor() {
+                private Integer n = 0;
 
-				@Override
-				public void visitAfter(final Element element, final ExecutionContext execution) {
-					n++;
+                @Override
+                public void visitAfter(final Element element, final ExecutionContext execution) {
+                    n++;
 
-					LOGGER.info("n == " + n);
-					LOGGER.info("element.getAttribute(\"number\") == " + element.getAttribute("number"));
-					LOGGER.info("element.getAttribute(\"truncated\") == " + element.getAttribute("truncated"));
+                    LOGGER.info("n == " + n);
+                    LOGGER.info("element.getAttribute(\"number\") == " + element.getAttribute("number"));
+                    LOGGER.info("element.getAttribute(\"truncated\") == " + element.getAttribute("truncated"));
 
-					NamedNodeMap attributes = element.getAttributes();
-					assertNotNull(attributes);
+                    NamedNodeMap attributes = element.getAttributes();
+                    assertNotNull(attributes);
 
-					LOGGER.info("attributes.getIndex(\"number\") == " + attributes.getNamedItem("number"));
-					LOGGER.info("attributes.getIndex(\"truncated\") == " + attributes.getNamedItem("truncated"));
+                    LOGGER.info("attributes.getIndex(\"number\") == " + attributes.getNamedItem("number"));
+                    LOGGER.info("attributes.getIndex(\"truncated\") == " + attributes.getNamedItem("truncated"));
 
-					for (int n = 0; n < attributes.getLength(); n++) {
-						LOGGER.info("attributes.getURI(" + n + ") == " + attributes.item(n).getBaseURI());
-						LOGGER.info("attributes.getLocalName(" + n + ") == " + attributes.item(n).getLocalName());
-						LOGGER.info("attributes.getQName(" + n + ") == " + attributes.item(n).getNodeName());
-						LOGGER.info("attributes.getType(" + n + ") == " + attributes.item(n).getNodeType());
-						LOGGER.info("attributes.getValue(" + n + ") == " + attributes.item(n).getNodeValue());
-					}
+                    for (int n = 0; n < attributes.getLength(); n++) {
+                        LOGGER.info("attributes.getURI(" + n + ") == " + attributes.item(n).getBaseURI());
+                        LOGGER.info("attributes.getLocalName(" + n + ") == " + attributes.item(n).getLocalName());
+                        LOGGER.info("attributes.getQName(" + n + ") == " + attributes.item(n).getNodeName());
+                        LOGGER.info("attributes.getType(" + n + ") == " + attributes.item(n).getNodeType());
+                        LOGGER.info("attributes.getValue(" + n + ") == " + attributes.item(n).getNodeValue());
+                    }
 
-					BeanContext beans = execution.getBeanContext();
-					assertNotNull(beans);
+                    BeanContext beans = execution.getBeanContext();
+                    assertNotNull(beans);
 
-					Data data = (Data) beans.getBean("Data");
-					assertNotNull(data);
+                    Data data = (Data) beans.getBean("Data");
+                    assertNotNull(data);
 
-					LOGGER.info("data.getNumber() == " + data.getNumber());
-					LOGGER.info("data.getTruncated() == " + data.getTruncated());
+                    LOGGER.info("data.getNumber() == " + data.getNumber());
+                    LOGGER.info("data.getTruncated() == " + data.getTruncated());
 
-					String number = element.getAttribute("number");
-					assertNotNull("number == null", number);
-					assertTrue("number.length() == 0", number.length() != 0);
-					assertEquals(number, n.toString());
+                    String number = element.getAttribute("number");
+                    assertNotNull("number == null", number);
+                    assertTrue("number.length() == 0", number.length() != 0);
+                    assertEquals(number, n.toString());
 
-					String truncated = element.getAttribute("truncated");
-					assertNotNull("truncated == null", truncated);
-					if (n == 1 || n == 4 || n == 7) {
-						assertEquals(truncated, "true");
-					} else {
-						assertEquals(truncated, "");
-					}
-				}
-			}, "record");
+                    String truncated = element.getAttribute("truncated");
+                    assertNotNull("truncated == null", truncated);
+                    if (n == 1 || n == 4 || n == 7) {
+                        assertEquals(truncated, "true");
+                    } else {
+                        assertEquals(truncated, "");
+                    }
+                }
+            }, "record");
 
-			StringResult result = new StringResult();
-			smooks.filterSource(smooks.createExecutionContext(), getSource(), result);
-			LOGGER.info(result.toString());
-		} finally {
-			if (smooks != null) {
-				smooks.close();
-			}
-		}
-	}
+            StringResult result = new StringResult();
+            smooks.filterSource(smooks.createExecutionContext(), getSource(), result);
+            LOGGER.info(result.toString());
+        } finally {
+            if (smooks != null) {
+                smooks.close();
+            }
+        }
+    }
 
-	private Source getSource() {
-		return new StreamSource(getClass().getResourceAsStream("/MILYN_427/data.flf"));
-	}
-	
+    private Source getSource() {
+        return new StreamSource(getClass().getResourceAsStream("/MILYN_427/data.flf"));
+    }
+
 }
